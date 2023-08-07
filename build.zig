c3dcdb8a93b6dde86b3d4722e51e8996c96ff082
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-08-07 21:58:22 +0100

    add build to function for adding second binary

--- build.zig
+++ build.zig
@@ -1,72 +1,44 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Add options during build time so we can use them during compile time
+pub fn build(b: *std.Build) !void {
+    try create_exe(b, "kilo", "src/kilo.zig");
+}
+
+fn create_exe(b: *std.Build, name: []const u8, root_path: []const u8) !void {
     const options = b.addOptions();
     options.addOption([]const u8, "kilo_version", "0.1.0");
-    options.addOption(usize, "kilo_tab_stop",8);
+    options.addOption(usize, "kilo_tab_stop", 8);
 
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "kilo-zig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/kilo.zig" },
+        .name = name,
+        .root_source_file = .{ .path = root_path },
         .target = target,
         .optimize = optimize,
     });
-    // link libc so we can use termios
     exe.linkLibC();
 
     // add options as a module. @import("kilo_options") will give us a struct with all the options we provided
     const m = options.createModule();
     exe.addModule("kilo_options", m);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
+    var buf: [32]u8 = undefined;
+    const run_name = try std.fmt.bufPrint(&buf, "run-{s}", .{name});
+    const run_step = b.step(run_name, "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/kilo.zig" },
+        .root_source_file = .{ .path = root_path },
         .target = target,
         .optimize = optimize,
     });
